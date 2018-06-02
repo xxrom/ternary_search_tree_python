ebfb6602a35b7debf7c590473aa2edee603923be
Chernyshov <xpom55@mail.ru>
2018-06-03 01:44:58 +0300

    add get method, was tested and everything WORKS FINE!!!

--- ternary_search_tree.py
+++ ternary_search_tree.py
@@ -10,10 +10,10 @@ class Node(object):
 class TernarySearchTree(object):
 
   def __init__(self):
-    self.rootNode = None
+    self.root = None
 
   def put(self, key, value):
-    self.rootNode = self.putItem(self.rootNode, key, value, 0)
+    self.root = self.putItem(self.root, key, value, 0)
 
   def putItem(self, node, key, value, index): # key[index] - path
     char = key[index]
@@ -43,6 +43,46 @@ class TernarySearchTree(object):
         index + 1 # спустились на уровень ниже посередине
       )
     else: # last index in the key
-      node.value = value # сохраняем новое значение
+      node.value = value # сохраняем value значение
+
+    return node
+
+  def get(self, key):
+    node = self.getItem(self.root, key, 0)
+
+    if node == None:
+      return -1 # не нашли значение !
+
+    return node.value # значит нашли значение
+
+  def getItem(self, node, key, index):
+    char = key[index]
+
+    if node == None:
+      return None # не нашли нужное значение
+
+    if node.character == char and index < len(key) - 1 : # совпадает значение
+      # увеличиваем index только в middle!!!
+      return self.getItem(node.middleNode, key, index + 1)
+
+    if char > node.character: # справа смотрим ребенка
+      return self.getItem(node.rightNode, key, index)
+
+    if char < node.character: # смотрим справа
+      return self.getItem(node.leftNode, key, index)
+
+    return node # нашли значение
 
 
+if __name__ == '__main__':
+  tst = TernarySearchTree()
+
+  tst.put('apple', 100)
+  tst.put('apper', 1001)
+  tst.put('appeturme', 10201)
+  tst.put('orange', 200)
+
+  print(tst.get('apple'))
+  print(tst.get('apper'))
+  print(tst.get('orange'))
+  print(tst.get('appeturme'))
